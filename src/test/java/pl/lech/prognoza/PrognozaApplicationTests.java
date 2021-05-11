52f3fab892ed702b80066d50000048f035c49f0f
lesio <lech.tula@onet.eu>
2021-05-11 17:34:25 +0200

    Initial

---
+++ src/test/java/pl/lech/prognoza/PrognozaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lech.prognoza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrognozaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
